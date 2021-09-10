9924ba7914f233804efe304a6809a78245f023cc
guilhermeulbriki <guilhermeulbriki@gmail.com>
2021-09-10 15:37:09 -0300

    finish thank you page

--- src/pages/thanks/index.tsx
+++ src/pages/thanks/index.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 
-import { Container } from './styles';
+import Footer from '../../components/footer';
+import Header from '../../components/header';
+
+import seta from '../../assets/seta.svg';
+import bolas from '../../assets/bolas.svg';
+
+import { Container, Content } from './styles';
 
 const Thanks: React.FC = () => {
   return (
     <Container>
-      <h1>Obriaghdo!</h1>
+      <Header />
+
+      <Content>
+        <div className="container">
+          <section className="up-header" />
+          <h1>Obrigado pela sua inscrição!</h1>
+          <p>
+            Como é bom te ter por aqui conosco. Seu cadastro foi concluído com
+            sucesso e logo entraremos em contato com você através do seu E-mail
+            com mais informações, fique ligado. Enquanto isso, que tal conhecer
+            um pouco mais sobre nós?
+          </p>
+          <img src={seta} alt="seta" />
+
+          <a target="_blank" rel="noreferrer" href="https://mooneyapp.com.br/">
+            <img src={bolas} alt="bolas" />
+            <button className="primary">Saiba mais sobre a Mooney</button>
+          </a>
+        </div>
+      </Content>
+
+      <Footer />
     </Container>
   );
 };
--- src/pages/thanks/styles.ts
+++ src/pages/thanks/styles.ts
@@ -1,3 +1,125 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-export const Container = styled.div``;
+const titleAnimation = keyframes`
+  from {
+    transform: translateX(-40px);
+  } to {
+    transform: translateX(0px);
+  }
+`;
+
+const textAnimation = keyframes`
+  from {
+    transform: translateX(-56px);
+  } to {
+    transform: translateX(0px);
+  }
+`;
+
+const buttonAnimation = keyframes`
+  from {
+    transform: translateY(24px);
+    opacity: 0;
+  } to {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+`;
+
+const setaAnimation = keyframes`
+  0% {
+    transform: translateY(0px);
+  } 50% {
+    transform: translateY(4px);
+  } 100% {
+    transform: translateY(0px);
+  }
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  overflow-x: hidden;
+  height: 100%;
+  min-height: 100vh;
+`;
+
+export const Content = styled.div`
+  height: calc(100vh - 8px - 8rem);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .up-header {
+      margin-bottom: 2.4rem;
+    }
+
+    h1 {
+      animation: ${titleAnimation} 600ms ease-out;
+    }
+
+    p {
+      animation: ${textAnimation} 900ms ease-out;
+    }
+
+    h1,
+    p {
+      width: 100%;
+      max-width: 800px;
+      text-align: center;
+    }
+
+    > img {
+      margin-top: 4.8rem;
+
+      animation: ${setaAnimation} linear infinite 1600ms;
+    }
+
+    a {
+      width: 100%;
+      max-width: 40.8rem;
+      position: relative;
+      margin-top: 8rem;
+
+      animation: ${buttonAnimation} ease-out 1200ms;
+
+      &:hover > img {
+        transform: translateY(-8px);
+      }
+
+      > img {
+        position: absolute;
+        top: 27px;
+        left: -15px;
+
+        transition: transform ease-out 600ms;
+      }
+    }
+  }
+
+  @media (max-width: 600px) {
+    .container {
+      h1 {
+        font-size: 2.8rem;
+      }
+
+      p {
+        font-size: 1.6rem;
+      }
+
+      > img {
+        margin-top: 4rem;
+      }
+
+      > a {
+        margin-top: 6.4rem;
+      }
+    }
+  }
+`;
